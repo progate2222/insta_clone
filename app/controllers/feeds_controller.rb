cd21ff308607c72178e370c25b4c03b24e787584
sayaka tamura <goooodnight@yahoo.co.jp>
2022-08-10 18:33:18 +0900

    確認画面の実装

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,83 +1,75 @@
 class FeedsController < ApplicationController
-  before_action :set_feed, only: %i[ show edit update destroy ]
+    before_action :set_feed, only: %i[ show edit update destroy ]
 
-  # GET /feeds or /feeds.json
-  def index
-    @feeds = Feed.all
-  end
-
-  # GET /feeds/1 or /feeds/1.json
-  def show
-    @favorite = current_user.favorites.find_by(feed_id: @feed.id)
-  end
-
-  # GET /feeds/new
-  def new
-    if params[:back]
-      @feed = Feed.new(feed_params)
-    else
-      @feed = Feed.new
+    def index
+        @feeds = Feed.all
     end
-  end
 
-  def confirm
-    @feed = current_user.feeds.build(feed_params)
-  end
-
-  # GET /feeds/1/edit
-  def edit
-    if current_user.email == @feed.user.email
-    else
-      redirect_to feeds_path
+    def show
+        @favorite = current_user.favorites.find_by(feed_id: @feed.id)
     end
-  end
 
-  # POST /feeds or /feeds.json
-  def create
-    @feed = current_user.feeds.build(feed_params)
-    respond_to do |format|
-      if @feed.save
-        format.html { redirect_to feed_url(@feed), notice: "Feed was successfully created." }
-        format.json { render :show, status: :created, location: @feed }
-        ContactMailer.send_mail(@feed).deliver
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
-      end
+    def new
+        if params[:back]
+            @feed = Feed.new(feed_params)
+        else
+            @feed = Feed.new
+        end
     end
-  end
 
-  # PATCH/PUT /feeds/1 or /feeds/1.json
-  def update
-    respond_to do |format|
-      if @feed.update(feed_params)
-        format.html { redirect_to feed_url(@feed), notice: "Feed was successfully updated." }
-        format.json { render :show, status: :ok, location: @feed }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
-      end
+    def confirm
+        @feed = current_user.feeds.build(feed_params)
     end
-  end
 
-
-  # DELETE /feeds/1 or /feeds/1.json
-  def destroy
-    @feed.destroy
-    respond_to do |format|
-      format.html { redirect_to feeds_url, notice: "Feed was successfully destroyed." }
-      format.json { head :no_content }
+    def edit
+        if current_user.email == @feed.user.email
+        else
+            redirect_to feeds_path
+            flash[:notice] = '一覧画面に遷移しました'
+        end
     end
-  end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
+    def create
+        @feed = current_user.feeds.build(feed_params)
+        respond_to do |format|
+            if @feed.save
+                format.html { redirect_to feed_url(@feed), notice: "Feed was successfully created." }
+                format.json { render :show, status: :created, location: @feed }
+                ContactMailer.send_mail(@feed).deliver
+            else
+                format.html { render :new, status: :unprocessable_entity }
+                format.json { render json: @feed.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def update
+        respond_to do |format|
+            if @feed.update(feed_params)
+                format.html { redirect_to feed_url(@feed), notice: "Feed was successfully updated." }
+                format.json { render :show, status: :ok, location: @feed }
+            else
+                format.html { render :edit, status: :unprocessable_entity }
+                format.json { render json: @feed.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    def destroy
+        @feed.destroy
+        respond_to do |format|
+            format.html { redirect_to feeds_url, notice: "Feed was successfully destroyed." }
+            format.json { head :no_content }
+        end
+    end
+
+    private
+
     def set_feed
-      @feed = Feed.find(params[:id])
+        @feed = Feed.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def feed_params
-      params.require(:feed).permit(:image, :image_cache, :content)
+        params.require(:feed).permit(:image, :image_cache, :content)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
 skip_before_action :login_required, only: [:new, :create]
 
     def new
@@ -26,15 +25,15 @@ skip_before_action :login_required, only: [:new, :create]
     def update
         @user = User.find(params[:id])
         respond_to do |format|
-          if @user.update(user_params)
-            format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-            format.json { render :show, status: :ok, location: @user }
-          else
-            format.html { render :edit, status: :unprocessable_entity }
-            format.json { render json: @user.errors, status: :unprocessable_entity }
-          end
+            if @user.update(user_params)
+                format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
+                format.json { render :show, status: :ok, location: @user }
+            else
+                format.html { render :edit, status: :unprocessable_entity }
+                format.json { render json: @user.errors, status: :unprocessable_entity }
+            end
         end
-      end
+    end
 
     private
 
